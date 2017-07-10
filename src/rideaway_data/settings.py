2e6c205ceed35871b407801f939c1bce5cb44ff5
Dieter Debast <dieter_debast@hotmail.com>
2017-07-10 13:31:35 +0200

    Allow server host

--- src/rideaway_data/settings.py
+++ src/rideaway_data/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$ip7*4waxh^9-@qlxq2@ohinisas1(9sipq!i)5gbb2fw$rxb3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['95.85.5.56']
 
 
 # Application definition
@@ -118,4 +118,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
