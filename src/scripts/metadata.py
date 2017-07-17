6aeb91ee34417886a90be065ae2f90dc721e7bff
Dieter Debast <dieter_debast@hotmail.com>
2017-07-17 16:08:33 +0200

    Metadata comparison (#21)
    * Compare metadata and place errors in geojson
    
    * Copy GFR file if OSM route is missing
    
    * Merge with master

---
+++ src/scripts/metadata.py
@@ -0,0 +1,40 @@
+import geojson, os
+from shutil import copyfile
+
+GFR_LOCATION = "../../data/gfr/"
+OSM_LOCATION = "../../data/osm/"
+TAGS_LOCATION = "../../data/tags/"
+MISSING_LOCATION = "../../data/missing/"
+
+
+def compare_tags(gfr, osm):
+    gfr_tags = gfr.features[0].properties
+    osm_tags = osm.features[0].properties
+    errors = ""
+
+    for key, value in gfr_tags.items():
+        if key not in osm_tags:
+            errors += "Missing tag: " + key + "=" + value + ";"
+        elif osm_tags[key] != value:
+            errors += "Wrong tag: " + key + "=" + osm_tags[key] + ", value should be:" + value + ";"
+
+    return errors
+
+
+for route_file in os.listdir(GFR_LOCATION):
+    if os.path.isfile(OSM_LOCATION + route_file):
+        with open(GFR_LOCATION + route_file) as fp:
+            gfr = geojson.loads(fp.read())
+
+        with open(OSM_LOCATION + route_file) as fp:
+            osm = geojson.loads(fp.read())
+
+        errors = compare_tags(gfr, osm)
+
+        if errors != "":
+            osm.features[0].properties['errors'] = errors
+
+        with open(TAGS_LOCATION + route_file, 'w') as fp:
+            fp.write(geojson.dumps(osm))
+    else:
+        copyfile(GFR_LOCATION + route_file, MISSING_LOCATION + route_file)
