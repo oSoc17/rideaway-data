276d13142f6276b19988c5eb8179268ff624a3d3
Dieter Debast <dieter_debast@hotmail.com>
2017-07-19 16:14:17 +0200

    Copy files to site (#28)
    * Fix small errors
    
    * Added logging
    
    * Log to stderr
    
    * Copy tags to difference file
    
    * Copy files to site folder

--- src/scripts/post_processor.py
+++ src/scripts/post_processor.py
@@ -1,9 +1,13 @@
-import os, geojson
-from shutil import copyfile
+import os, geojson, errno
+from shutil import copyfile, rmtree
 from constants import *
 
 
-def merge_differences(missing_file, wrong_file, output):
+def merge_differences(route, missing_file, wrong_file, output):
+    with open(TAGS_LOCATION + route) as fp:
+        tags = geojson.loads(fp.read())
+        properties = tags.features[0].properties
+
     with open(missing_file) as fp:
         missing = geojson.loads(fp.read())
 
@@ -12,6 +16,7 @@ def merge_differences(missing_file, wrong_file, output):
 
     if len(missing.features[0].geometry.coordinates) > 0 and len(wrong.features[0].geometry.coordinates) == 0:
         missing.features[0].properties['difference_type'] = 'missing'
+        missing.features[0].properties.update(properties)
 
         with open(output, 'w') as fp:
             fp.write(geojson.dumps(missing))
@@ -19,6 +24,7 @@ def merge_differences(missing_file, wrong_file, output):
         return True
     elif len(wrong.features[0].geometry.coordinates) > 0 and len(missing.features[0].geometry.coordinates) == 0:
         wrong.features[0].properties['difference_type'] = 'wrong'
+        wrong.features[0].properties.update(properties)
 
         with open(output, 'w') as fp:
             fp.write(geojson.dumps(wrong))
@@ -26,7 +32,9 @@ def merge_differences(missing_file, wrong_file, output):
         return True
     elif len(missing.features[0].geometry.coordinates) > 0 and len(wrong.features[0].geometry.coordinates) > 0:
         missing.features[0].properties['difference_type'] = 'missing'
+        missing.features[0].properties.update(properties)
         wrong.features[0].properties['difference_type'] = 'wrong'
+        wrong.features[0].properties.update(properties)
 
         with open(output, 'w') as fp:
             fp.write(geojson.dumps(geojson.FeatureCollection([missing.features[0], wrong.features[0]])))
@@ -36,14 +44,45 @@ def merge_differences(missing_file, wrong_file, output):
         return False
 
 
+def copy_to_site():
+    if os.path.exists(SITE_GFR):
+        rmtree(SITE_GFR)
+
+    try:
+        os.makedirs(SITE_GFR)
+    except OSError as e:
+        # We don't care if it already exists although it shouldn't exist
+        if e.errno != errno.EEXIST:
+            raise
+
+    for route in os.listdir(GFR_ROUTES_LOCATION):
+        copyfile(GFR_ROUTES_LOCATION + route, SITE_GFR + route)
+
+    if os.path.exists(SITE_OUTPUT):
+        rmtree(SITE_OUTPUT)
+
+    try:
+        os.makedirs(SITE_OUTPUT)
+    except OSError as e:
+        # We don't care if it already exists although it shouldn't exist
+        if e.errno != errno.EEXIST:
+            raise
+
+    for route in os.listdir(OUTPUT_LOCATION):
+        copyfile(OUTPUT_LOCATION + route, SITE_OUTPUT + route)
+
+
 def post_process():
     for route in os.listdir(GFR_ROUTES_LOCATION):
         if os.path.isfile(MISSING_LOCATION + route):
             copyfile(MISSING_LOCATION + route, OUTPUT_LOCATION + route)
         elif os.path.isfile(DIFF_MISSING_LOCATION + route) and os.path.isfile(DIFF_WRONG_LOCATION + route) \
-                and merge_differences(DIFF_MISSING_LOCATION + route, DIFF_WRONG_LOCATION + route, OUTPUT_LOCATION + route):
+                and merge_differences(route, DIFF_MISSING_LOCATION + route, DIFF_WRONG_LOCATION + route,
+                                      OUTPUT_LOCATION + route):
             pass
         elif os.path.isfile(TAGS_LOCATION + route):
             copyfile(TAGS_LOCATION + route, OUTPUT_LOCATION + route)
         else:
             raise Exception("No output file could be generated for route: " + route)
+
+    copy_to_site()
