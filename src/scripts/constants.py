6d438d21d35522f2a55babfee2764a537f9c1cab
Dieter Debast <dieter_debast@hotmail.com>
2017-07-20 14:45:59 +0200

    Generate complete network for BruMob2

--- src/scripts/constants.py
+++ src/scripts/constants.py
@@ -14,9 +14,11 @@ MISSING_LOCATION = "../data/missing/"
 DIFF_MISSING_LOCATION = "../data/diff/missing/"
 DIFF_WRONG_LOCATION = "../data/diff/wrong/"
 OUTPUT_LOCATION = "../data/output/"
+NETWORK_OUTPUT = "../data/network.geojson"
 
 SITE_OUTPUT = "../brumob/templates/brumob/data/output/"
 SITE_GFR = "../brumob/templates/brumob/data/routes/"
+SITE_NETWORK = "../brumob/templates/brumob/data/network.geojson"
 
 DATA_FOLDERS = [OSM_ROUTES_LOCATION, GFR_ROUTES_LOCATION, TAGS_LOCATION, MISSING_LOCATION, DIFF_MISSING_LOCATION,
                 DIFF_WRONG_LOCATION, OUTPUT_LOCATION]
