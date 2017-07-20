35382d8534461d2134be777be606a3378d4b0987
Dieter Debast <dieter_debast@hotmail.com>
2017-07-20 15:15:51 +0200

    Make network available for download

--- src/brumob/views.py
+++ src/brumob/views.py
@@ -24,8 +24,8 @@ def downloads(request):
     template = loader.get_template('brumob/download.html')
 
     #
-    route_list=["1","1a","1b","2","2a","2b","3","3a","3b","4","4a","4b","5","5a","5b","MM","6","6a","6b","7","8","SZ","SZa","SZb","KC","9","9a","9b","10","10a","10b","11","11a","11b","12","12a","12b","MM","MMa","MMb","PP","A","B","C"]
-    output_list=["1","1a","1b","2","2a","2b","3","3a","3b","4","4a","4b","5","5a","5b","MM","6","6a","6b","7","8","SZ","SZa","SZb","KC","9","9a","9b","10","10a","10b","11","11a","11b","12","12a","12b","MM","MMa","MMb","PP","A","B","C"]
+    route_list=["1","1a","1b","2","2a","2b","3","3a","3b","4","4a","4b","5","5a","5b","MM","6","6a","6b","7","8","SZ","SZa","SZb","CK","9","9a","9b","10","10a","10b","11","11a","11b","12","12a","12b","MM","MMa","MMb","PP","A","B","C"]
+    output_list=["1","1a","1b","2","2a","2b","3","3a","3b","4","4a","4b","5","5a","5b","MM","6","6a","6b","7","8","SZ","SZa","SZb","CK","9","9a","9b","10","10a","10b","11","11a","11b","12","12a","12b","MM","MMa","MMb","PP","A","B","C"]
 
     context={
     'route_list':route_list,
@@ -53,4 +53,14 @@ def downloadDifferenceItem(request,route):
     with open(os.path.join(BASE, "templates/brumob/data/output/"+str(route)+".geojson"), 'rb') as fh:
         response = HttpResponse(fh, content_type="application/json") 
         response["Content-disposition"] = "attachment; filename="+str(route)+".geojson"
+        return response
+
+def downloadNetwork(request):
+    # template = loader.get_template('brumob/download.html')
+    # context={}
+
+    BASE = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(BASE, "templates/brumob/data/network.geojson"), 'rb') as fh:
+        response = HttpResponse(fh, content_type="application/json")
+        response["Content-disposition"] = "attachment; filename=network.geojson"
         return response
