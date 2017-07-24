ce39790d3c47ccfed02a7cf621c26e781ce026a7
Moustapha Ramachi <moustapharamachi@Moustaphas-MacBook-Air.local>
2017-07-24 10:54:50 +0200

    small clean-up

--- nts/Program.cs
+++ nts/Program.cs
@@ -9,12 +9,20 @@ namespace NTS_BufferingTest
     {
         static void Main(string[] args)
         {
+            /*
+            3 inputs are required, 2 files to compare and one output
+            It compares it geometric overlaap between the first 2 inputs and uses the third for output
+            A buffered version of the first input is compared to the second input
+            The output is the geometric difference between the inputs 
+            */
+
             var serialize = new NetTopologySuite.IO.GeoJsonSerializer();
             var inputFile1 = File.OpenText(args[0]);
             var inputFile2 = File.OpenText(args[1]);
             var features1 = serialize.Deserialize<FeatureCollection>(new JsonTextReader(inputFile1));
             var features2 = serialize.Deserialize<FeatureCollection>(new JsonTextReader(inputFile2));
 
+            //buffer first input
             var bufferedFeatures = new FeatureCollection();
             foreach(var feature in features1.Features)
             {
@@ -25,17 +33,7 @@ namespace NTS_BufferingTest
 
                 bufferedFeatures.Add(bufferedFeature);
             }
-
-            //var intersections = new FeatureCollection();
-            //foreach(var feature in bufferedFeatures.Features)
-            //{
-            //    foreach(var feature2 in features2.Features)
-            //    {
-            //        var intersection = feature.Geometry.Intersection(feature2.Geometry);
-            //        intersections.Add(new Feature(intersection, new AttributesTable()));
-            //    }
-            //}
-
+            //extract all differences by comparing a feuture from input 1 with all features from input 2
             var differences = new FeatureCollection();
             foreach (var feature in bufferedFeatures.Features)
             {
@@ -46,17 +44,7 @@ namespace NTS_BufferingTest
                 }
             }
 
-            Console.WriteLine(differences.Features.Count.ToString());
-            File.Delete("buffered.geojson");
-            using (var outputFile = new StreamWriter(File.OpenWrite("buffered.geojson")))
-            {
-                serialize.Serialize(outputFile, bufferedFeatures);
-            }
-            //File.Delete(args[2]);
-            //using (var outputFile = new StreamWriter(File.OpenWrite(args[2])))
-            //{
-            //    serialize.Serialize(outputFile, intersections);
-            //}
+           //output file created
             File.Delete(args[2]);
             using (var outputFile = new StreamWriter(File.OpenWrite(args[2])))
             {
